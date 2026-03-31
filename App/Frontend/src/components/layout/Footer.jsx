78a42dfeca5424df13151984e17ab6aa74235289
Carlos Sánchez <carlosasanchez1976@gmail.com>
2026-03-31 17:29:45 -0300

    Ajuste a estilos css segun solicitud

--- App/Frontend/src/components/layout/Footer.jsx
+++ App/Frontend/src/components/layout/Footer.jsx
@@ -1,4 +1,5 @@
 import { FaInstagram, FaFacebook } from 'react-icons/fa';
+import cpauLogo from '../../assets/images/cpau.svg';
 import styles from './Footer.module.css';
 
 /**
@@ -8,8 +9,16 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
+        <div className={styles.footerLeft}>
+          <img 
+            src={cpauLogo} 
+            alt="Logo CPAU" 
+            className={styles.footerLogo}
+          />
+        </div>
+        
         <p className={styles.copyright}>
-          © 2026 - Desarrollado por neosis para el CPAU
+          © Consejo Profesional de Arquitectura y Urbanismo. 25 de Mayo 482. C1002ABJ CABA. Tel: +5411 5238 1068. Email: rect@cpau.org. Atención: lunes a viernes de 9 a 16 horas.
         </p>
         
         <div className={styles.socialLinks}>
